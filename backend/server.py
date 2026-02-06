e82e260a16382c467e920205313b8c58bff5d98b
samasti61 <fatihzeynephamza@gmail.com>
2026-02-06 17:01:00 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ import uuid
 from datetime import datetime, timezone, timedelta
 from passlib.context import CryptContext
 from jose import JWTError, jwt
-from emergentintegrations.llm.chat import LlmChat, UserMessage
+# from emergentintegrations.llm.chat import LlmChat, UserMessage
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
